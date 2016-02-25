8ccd0e664b9221b1c94ee7ada59bcdd65438c7d0
Rajesh Babu <trcrajesh_be@yahoo.com>
2016-02-24 19:18:38 -0500

    Create modern-web-init.js

---
+++ modern-web-ui-core/modern-web-init/assets/js/modern-web-init.js
@@ -0,0 +1,32 @@
+// Configure
+require.config({
+  baseUrl: 'modern-web-ui-core/',
+
+  // Create shortcuts to all libraries
+  paths: {
+
+    // Vendor libraries
+    'modernizr':                  'empty:', // in header 'modern-web-vendor-modernizr/assets/js/modernizr.min',
+    'jquery':                     'empty:', // in header 'modern-web-vendor-jquery/assets/js/jquery.min',
+    'bootstrap':                  'modern-web-vendor-bootstrap/assets/js/bootstrap',
+    'bootstrap-accessibility':    'modern-web-vendor-bootstrapaccessibility/assets/js/bootstrap-accessibility.min',
+
+    // Internal libraries
+    'attachJQ':                   'modern-web-init/assets/js/attachJQ',
+	'utilities':                  'modern-web-js-utilities/assets/js',
+
+    // Components
+    'form-search':                'modern-web-ui-form-search/assets/js/modern-web-ui-form-search',
+
+    'f2':                         'modern-web-f2/assets/js/modern-web-f2'
+  },
+
+  // Wrap non-AMD libraries
+  shim: {
+    'f2' : {deps: ['jquery'], exports :"f2"}
+  },
+
+  // Load init
+  deps: [ 'modern-web-init/assets/js/load.js' ]
+
+});
